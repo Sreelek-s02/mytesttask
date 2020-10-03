894b8499f7b766bff46850c9b574261da35a832e
Sreelekshmi <leksree94@gmal.com>
2020-10-03 20:45:11 +0530

    get  details from stac overflow and display

--- iqm/src/App.js
+++ iqm/src/App.js
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, { Component, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { AgGridColumn, AgGridReact } from 'ag-grid-react';
 
-function App() {
+
+class App extends React.Component {
+  state = {
+    questions: [] 
+  }
+componentDidMount(){
+  axios.get("https://api.stackexchange.com/2.2/questions?site=stackoverflow")
+      .then(res => {
+        //  const questions =[];
+        console.log(res.data)
+        this.setState({ questions: res.data.items  });
+      })
+
+}
+
+  render(){
+    // const data =[{"0": "auth1","1": "auth2"}]
+    console.log("qns",this.state.questions)
+const data = this.state.questions;
+console.log("data",data);
+  
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="col-12" >
+       <table className="table"> 
+       <thead> 
+       <tr>
+         <th>Author</th>
+         <th>Title</th>
+         <th>Creation Date</th>
+         </tr> 
+         </thead>
+         <tbody>
+           {data.map(item => 
+           <tr key={item.id}>
+             <td>{item.owner.display_name}</td>
+             <td>{item.title}</td>
+             <td>{item.creation_date}</td>
+
+           </tr>)}
+         </tbody>
+       </table>  
+        </div>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,9 +60,10 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
+}
 }
 
 export default App;
